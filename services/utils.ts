c278e79fba83c21cfb3ad0f3d68663e8c679291e
Alex Kreidler <alexkreidler2020@gmail.com>
2021-04-07 17:15:05 -0400

    add configs

--- services/utils.ts
+++ services/utils.ts
@@ -1,14 +1,16 @@
-import { Stream } from "stream";
+import { Stream } from 'stream'
 export function streamToString(stream: Stream) {
-    console.log("h", stream);
-    
-  const chunks: any[] = [];
+  console.log('h', stream)
+
+  const chunks: any[] = []
   return new Promise((resolve, reject) => {
-    stream.on("data", (chunk) => {
-      console.log(chunk);
-      chunks.push(Buffer.from(chunk));
-    });
-    stream.on("error", (err) => reject(err));
-    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-  });
+    stream.on('data', (chunk) => {
+      console.log(chunk)
+      chunks.push(Buffer.from(chunk))
+    })
+    stream.on('error', (err) => reject(err))
+    stream.on('end', () =>
+      resolve(Buffer.concat(chunks).toString('utf8'))
+    )
+  })
 }
